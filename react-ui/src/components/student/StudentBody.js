7745689dab660ab8320c236478aea7ded3e44804
SamuelPapyan <samuel.papyan170@gmail.com>
2024-06-23 15:54:30 +0400

    feat: just added temporary supporeted chatbot

--- react-ui/src/components/student/StudentBody.js
+++ react-ui/src/components/student/StudentBody.js
@@ -4,8 +4,22 @@ import { Outlet } from "react-router"
 import ChatBox from "../common/ChatBox";
 
 import '../../style/App.css';
+import { useEffect } from "react";
 
 export default function StudentBody(props) {
+
+    useEffect(()=>{
+        if (window.localStorage.getItem(process.env.REACT_APP_STUDENT_TOKEN)) {
+            window.__be = window.__be || {};
+            window.__be.id = "6675beb7092d0b00071836e4";
+            (function() {
+                var be = document.createElement('script'); be.type = 'text/javascript'; be.async = true;
+                be.src = ('https:' == document.location.protocol ? 'https://' : 'http://') + 'cdn.chatbot.com/widget/plugin.js';
+                var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(be, s);
+            })();
+        }
+    })
+
     return (
         <>
             <div className="text-center">
